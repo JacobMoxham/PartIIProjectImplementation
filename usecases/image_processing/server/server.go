b44114ff14dcf06a7c69ae7ea8c7ef7467fdb302
Jacob Moxham <jacobmoxham@live.co.uk>
2019-02-14 19:38:39 +0000

    move all image processing to image processing package

--- usecases/image_processing/server/server.go
+++ usecases/image_processing/server/server.go
@@ -4,42 +4,12 @@ import (
 	"fmt"
 	. "github.com/JacobMoxham/PartIIProjectImplementation/image_recognition"
 	"github.com/JacobMoxham/PartIIProjectImplementation/middleware"
-	"github.com/tensorflow/tensorflow/tensorflow/go"
 	"log"
 	"net/http"
 )
 
 func imageProcessingHandler(w http.ResponseWriter, r *http.Request) {
-	modelGraph, labels, err := LoadModel()
-	if err != nil {
-		log.Fatalf("unable to load model: %v", err)
-	}
-
-	// Get normalized tensor
-	tensor, err := NormalizeImage(r.Body)
-	if err != nil {
-		log.Fatalf("unable to make a tensor from image: %v", err)
-	}
-
-	// Create a session for inference over modelGraph
-	session, err := tensorflow.NewSession(modelGraph, nil)
-	if err != nil {
-		log.Fatalf("could not init session: %v", err)
-	}
-
-	output, err := session.Run(
-		map[tensorflow.Output]*tensorflow.Tensor{
-			modelGraph.Operation("input").Output(0): tensor,
-		},
-		[]tensorflow.Output{
-			modelGraph.Operation("output").Output(0),
-		},
-		nil)
-	if err != nil {
-		log.Fatalf("could not run inference: %v", err)
-	}
-
-	res := GetTopFiveLabels(labels, output[0].Value().([][]float32)[0])
+	getTop5Labels := GetTopFiveLabelsFromImageReader(r.Body)
 	returnString := ""
 	for _, l := range res {
 		fmt.Printf("label: %s, probability: %.2f%%\n", l.Label, l.Probability*100)
