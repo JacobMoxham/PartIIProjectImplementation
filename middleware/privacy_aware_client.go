44bfea84332e8153ac87cd946b6eec80e94df12c
Jacob Moxham <jacobmoxham@live.co.uk>
2019-02-16 13:45:08 +0000

    added untested version of ip conversion

--- middleware/privacy_aware_client.go
+++ middleware/privacy_aware_client.go
@@ -1,16 +1,19 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 type PrivacyAwareClient struct {
 	client            *http.Client
-	computationPolicy *ComputationPolicy
+	computationPolicy ComputationPolicy
 }
 
 func MakePrivacyAwareClient(policy ComputationPolicy) PrivacyAwareClient {
 	return PrivacyAwareClient{
 		client:            &http.Client{},
-		computationPolicy: &policy,
+		computationPolicy: policy,
 	}
 }
 
@@ -25,6 +28,30 @@ func (c PrivacyAwareClient) Send(req PamRequest) (PamResponse, error) {
 
 	// TODO: consider whether we can handle this if we have all of the data and can process locally.
 	//  This may require us to specify a dependency on "has all data" when registering handlers, I'll have a think
+	// Currently not having a dependency in the handler
+	// TODO: Also ignoring the fact the ports may need to change
+
+	// Check if we would prefer to process locally
+	policy := req.Policy
+	preferLocal := policy.PreferredProcessingLocation == Local
+
+	// Check if we can process this request locally
+	requestPath := req.HttpRequest.URL.Path
+	computationPolicy := c.computationPolicy
+	computationLevel, _ := computationPolicy.Resolve(requestPath, Local)
+
+	// Check if we have all of the required data to process this locally within the request
+	allRequiredData := req.Policy.HasAllRequiredData
+
+	// Change the host to be 127.0.0.1 (localhost) if we can process locally
+	if preferLocal && computationLevel != NoComputation && allRequiredData {
+		hostAndPort := httpRequest.URL.Host
+		hostAndPortSplit := strings.Split(hostAndPort, ":")
+		hostAndPortSplit[0] = "127.0.0.1"
+		hostAndPort = strings.Join(hostAndPortSplit, ":")
+
+		httpRequest.URL.Host = hostAndPort
+	}
 
 	resp, err := c.client.Do(httpRequest)
 	if err != nil {
---
+++ middleware/privacy_aware_client_test.go
@@ -0,0 +1,3 @@
+package middleware
+
+// TODO: test ip conversion... quite hard to do locally
