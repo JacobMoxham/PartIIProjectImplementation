3942742a2de32bddd8c60f5577eb1b911f264b19
Jacob Moxham <jacobmoxham@live.co.uk>
2019-02-20 23:54:18 +0000

    tidied up comp policy usecase

--- usecases/aggregate_power_consumption_comp_policies/dep-ensure-all.sh
+++ usecases/aggregate_power_consumption_comp_policies/dep-ensure-all.sh
@@ -3,13 +3,5 @@ cd server/
 dep ensure -update
 cd ../requesting-client/
 dep ensure -update
-cd ../data-client-both/
-dep ensure -update
-cd  ../data-client-can-compute/
-dep ensure -update
-cd ../data-client-no-computation/
-dep ensure -update
-cd ../data-client-raw-data/
-dep ensure -update
 cd ../data-client-configurable/
 dep ensure -update
